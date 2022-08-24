d6c8b7aae4d886b5bc6528f06f0fb1d6fab8dc43
SinkLine_P <popovartem505@gmail.com>
2022-08-24 03:04:45 +0300

    Create backup formatter 9

--- 7/index.js
+++ 7/index.js
@@ -5,6 +5,15 @@ function decoder(string) {
   const splitStr = string.split(regex);
   const newArray = [];
 
+  const oneLine = splitStr[0];
+  const firstLitters = oneLine.split("")[0];
+  const centerLitters = oneLine.split("")[1];
+  const secondLitters = oneLine.split("")[2];
+
+  if (firstLitters === secondLitters && centerLitters !== firstLitters && centerLitters !== secondLitters) {
+
+  }
+
 
 }
 
